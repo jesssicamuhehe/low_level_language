0e32b79d52d7f5683c2442a2735551193bad7367
jessicamuhehe <jessicamuhehe@gmail.com>
2023-06-06 13:20:56 -0700

    C programming variables_if_else_while scripts

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- *  * main - print the last digit of the number stored in the variable followed by a new line.
+ *  * main - print the last digit of the number stored in the variable.
  *   *
  *    * Return: Always 0 (Success)
  */
@@ -11,25 +11,20 @@ int main(void)
 {
 		int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
 
-					if ((n % 10) > 5)
-							{
-										printf("Last digit of %d is %d and is greater than 5\n",
-															n, n % 10);
-											}
-						else if ((n % 10) < 6 && (n % 10) != 0)
-								{
-											printf("Last digit of %d is %d and is less than 6 and not 0\n",
-																n, n % 10);
-												}
-							else
-									{
-												printf("Last digit of %d is %d and is 0\n",
-																	n, n % 10);
-													}
-
-								return (0);
+		if ((n % 10) > 5)
+		{
+			printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+		else if ((n % 10) < 6 && (n % 10) != 0)
+		{
+			printf("Last digit of %d is %d and is less than 6 and not 0\n",	n, n % 10);
+		}
+		else
+		{
+			printf("Last digit of %d is %d and is 0\n", n, n % 10);
+		}
+		return (0);
 
 }
